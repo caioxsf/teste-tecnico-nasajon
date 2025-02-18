e92d4fe8fb058c8632ba67bc624d209845262694
caioxsf <caio.nobile@hotmail.com>
2025-02-18 12:39:42 -0300

    feat: adicionando questao 1

---
+++ 1)/teste_1.js
@@ -0,0 +1,13 @@
+let indice = 13;
+let soma = 0;
+let k =0;
+
+function valorDaSoma () {
+    while (k < indice) {
+        k += 1;
+        soma += k;
+    }
+    console.log(soma);
+}
+
+valorDaSoma();
